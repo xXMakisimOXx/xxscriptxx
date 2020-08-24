b46f52758adfadaf9cf43e853427c6d515734716
xXMakisimOXx <70127484+xXMakisimOXx@users.noreply.github.com>
2020-08-24 23:21:25 +0200

    Update background-update.php

--- background-update.php
+++ background-update.php
@@ -1,1 +1,8 @@
-
+// переименование файла
+$file = 'background/2.png';
+$new_name = '321312.png';
+if (rename($file, $new_name)) {
+    echo "Файл успешно переименован!";
+}else{
+    echo "Файл не удалось переименовать!";
+}
